6ce9cffde1ed7a27753e8605001bf28c17cc5113
David Thomas <sjufan84@gmail.com>
2022-06-18 15:58:24 -0700

    added basic streamlit app framework

---
+++ streamlit_app.py
@@ -0,0 +1,26 @@
+from matplotlib.pyplot import legend
+import pandas as pd
+import streamlit as st
+import hvplot.pandas
+import geoviews as gv
+from pathlib import Path
+import holoviews as hv
+from geoviews import dim, opts
+
+st.
+st.header('Areas of opportunity in Real Estate Based on Projected Population Migration Patterns within the US due to Climate Change')
+
+new_combined_df = pd.read_csv(Path('./Resources/new_combined_df.csv'), index_col = [0])
+
+new_combined_df['NAME'] = new_combined_df.index
+data = gv.Dataset(new_combined_df, kdims=['Population_Delta', 'Population_Price_Discrepancy', 'NAME', '2019_home_values'])
+tiles = gv.tile_sources.Wikipedia
+points = data.to(gv.Points, ['LONGITUDE', 'LATITUDE'], ['Population_Delta', 'Population_Price_Discrepancy', 'NAME', '2019_home_values'])
+fig = (tiles * points).opts(
+    opts.Points(width=600, height=350, tools=['hover'], size=gv.dim('Population_Delta')*100,
+                color='2019_home_values', cmap='reds', title = 'Counties expected to gain >= 7.5% population by 2025', show_legend=True))
+analytics_df = pd.DataFrame(new_combined_df.iloc[:, -6:])
+
+st.bokeh_chart(hv.render(fig))
+st.markdown('**Size of point indicates population delta, shade of red indicates recent median home values**')
+st.table(analytics_df)
