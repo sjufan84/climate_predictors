1753c086d46c24987a901e2fe549d17250d13591
David Thomas <sjufan84@gmail.com>
2022-06-19 13:17:00 -0700

    converted graphs to plotly, cleaned up Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,30 @@
 import pandas as pd
 import streamlit as st
-import geoviews as gv
 from pathlib import Path
-import holoviews as hv
-from geoviews import opts
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-gv.extension('bokeh')
 
-st.header('Areas of opportunity in Real Estate Based on Projected Population Migration Patterns within the US due to Climate Change')
+import plotly.express as px
+
+load_dotenv()
+mapbox_access_token = os.getenv('MAPBOX_ACCESS_TOKEN')
+
+
+st.markdown('#### *Map is interactive.*  **Size of point indicates population delta, shade of red indicates recent median home values.**')
 
 new_combined_df = pd.read_csv(Path('./Resources/new_combined_df.csv'), index_col = [0])
 
-new_combined_df['NAME'] = new_combined_df.index
-data = gv.Dataset(new_combined_df, kdims=['Population_Delta', 'Population_Price_Discrepancy', 'NAME', '2019_home_values'])
-tiles = gv.tile_sources.Wikipedia
-points = data.to(gv.Points, ['LONGITUDE', 'LATITUDE'], ['Population_Delta', 'Population_Price_Discrepancy', 'NAME', '2019_home_values'])
-fig = (tiles * points).opts(
-    opts.Points(width=600, height=350, tools=['hover'], size=gv.dim('Population_Delta')*100,
-                color='2019_home_values', cmap='reds', title = 'Counties expected to gain >= 7.5% population by 2025', show_legend=True))
+fig = px.scatter_mapbox(new_combined_df, lat="LATITUDE", lon="LONGITUDE", color="2019_home_values", size = 'Population_Delta_Raw', labels = {'2019_home_values' : 'Median Home Values'},
+                        color_continuous_scale='reds', zoom=2.25, mapbox_style="open-street-map", center = {'lon': -97, 'lat' : 37.11}, height = 500, width = 750, 
+                        hover_data = ['NAME.1', '2019_home_values', 'Population_Delta', 'Population_Delta_Raw'], title = '2025 Migration Projections and Home Value Data by County')
+
+
+st.plotly_chart(fig)
+
+
 analytics_df = pd.DataFrame(new_combined_df.iloc[:, -6:])
 
-st.bokeh_chart(hv.render(fig))
-st.markdown('**Size of point indicates population delta, shade of red indicates recent median home values**')
+st.markdown('#### Table below contains data for all counties in the US projected to increase in size by 7.5% or more by 2025 (table can be expanded)')
 st.table(analytics_df)
